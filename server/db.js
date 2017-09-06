c681205c7ed8004704857bc81a388d51e6d4a98d
hellomrbigshot <1994yechen@gmail.com>
2017-09-06 23:53:39 +0800

    use mongodb to login

---
+++ server/db.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose'),
+	  DB_URL = 'mongodb://localhost:27017/vue-oc';
+
+mongoose.connect(DB_URL)
+
+mongoose.connection.on('connected', () => { 
+	console.log('connect success')
+})
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+	username: {type: String, required: true},
+	userpwd: {type: String, required: true},
+	userage: {type: Number},
+	logindate: {type: Date}
+})
+
+const User = module.exports = mongoose.model('User', UserSchema)
+// const user = new User({
+// 	username: '王迪',
+// 	userpwd: 'helloworld',
+// 	userage: '25',
+// 	logindate: new Date()
+// })
+
+// user.save( (err, res) => {
+// 	// body...
+// 	if(err) {
+// 		console.log(`Err: ${err}`);
+// 	} else {
+// 		console.log(`Res: ${res}`);
+// 	}
+// })
+
+// User.find({username: '王迪', _id: '59aff9126625030c049f358c'}, (err, res) => {
+// 	if(err) {
+// 		console.log(`Err: ${err}`);
+// 	} else {
+// 		// console.log(`Res: ${res}`);
+// 		console.log(res[0])
+// 	}
+// })
+
+
+
+
+      
